3c784d847d64d1f00c8cb43942028771ab6c644a
khanqahequadriya <khanqahequadriya@gmail.com>
2018-07-18 16:57:48 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -81,9 +81,12 @@
     };
 
 // preloader
-$(window).on('load',function(){
-  $('.spinner').fadeOut('slow',function(){
-    $('.preloader').remove();
-    $("nav").addClass('fixed-top');
-  });
-});
+$(document).ready(function(){
+    $('main').hide();
+    $(window).on({
+        load: function(){
+            $('.preloader').hide();
+            $('main').show();
+        }
+    })
+})
