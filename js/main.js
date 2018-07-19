63c4746f4d821065e03dff1f0abf2e86ecd579bc
khanqahequadriya <khanqahequadriya@gmail.com>
2018-07-20 00:00:56 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -9,27 +9,27 @@
 // });
 
 // // carousel
-// $('.owl-carousel').owlCarousel({
+$('.owl-carousel').owlCarousel({
 
-//     loop: true,
-//     margin: 8,
-//     autoplay: true,
-//     autoplayTimeout: 2000,
-//     autoplayHoverPause: false,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 3
-//         },
-//         1000: {
-//             items: 4
-//         }
-//     }
-// });
+    loop: true,
+    margin: 8,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
 
-// // form validation
+// form validation
 // $.validate({
 //     lang: 'en'
 // });
